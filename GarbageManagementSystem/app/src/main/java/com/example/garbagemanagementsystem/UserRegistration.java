500d381e8b8c4de700925e57d45c5813a6687bd5
hIFZA624 <mcsf19m020@pucit.edu.pk>
2021-05-24 13:19:59 +0500

    RegisteChecks

--- GarbageManagementSystem/app/src/main/java/com/example/garbagemanagementsystem/UserRegistration.java
+++ GarbageManagementSystem/app/src/main/java/com/example/garbagemanagementsystem/UserRegistration.java
@@ -24,25 +24,66 @@ EditText FirstName;
         Email=findViewById(R.id.email);
         password=findViewById(R.id.Password);
         ConfirmPassword=findViewById(R.id.confirmPassword);
+        if(FirstName.getText().toString().trim().length()!=0)
+        {
+            FirstName.setError(null);
+        }
+        if(LastName.getText().toString().trim().length()!=0)
+        {
+            LastName.setError(null);
+        }
+        if(Email.getText().toString().trim().length()!=0)
+        {
+            Email.setError(null);
+        }
+        if(password.getText().toString().trim().length()!=0)
+        {
+            password.setError(null);
+        }
     }
     public void RegisterUser(View view) {
         /*DbHelper dbHelper = new DbHelper(UserRegistration.this);
         List<User> allcustomers=dbHelper.getAllRecords();
         Toast.makeText(UserRegistration.this,allcustomers.toString(), Toast.LENGTH_LONG).show();*/
-        if(password==ConfirmPassword) {
-            User user = new User(FirstName.getText().toString(), LastName.getText().toString(), Email.getText().toString(), password.getText().toString(), ConfirmPassword.getText().toString(), 1);
-            // Toast.makeText(UserRegistration.this, user.toString(), Toast.LENGTH_SHORT).show();
-            DbHelper dbHelper = new DbHelper(UserRegistration.this);
-            boolean b = dbHelper.addUser(user);
-            if (b == true)
-                Toast.makeText(UserRegistration.this, "uSER aDDED", Toast.LENGTH_SHORT).show();
-            else
-                Toast.makeText(UserRegistration.this, "User Not ADDED", Toast.LENGTH_SHORT).show();
-        }
-        else
+        if(FirstName.getText().toString().trim().length()==0)
         {
-            Toast.makeText(UserRegistration.this, "Password Not Matched", Toast.LENGTH_LONG).show();
+            FirstName.setError("Enter First Name");
         }
 
+        if(LastName.getText().toString().trim().length()==0)
+        {
+            LastName.setError("Enter Last Name");
+        }
+
+        if(Email.getText().toString().trim().length()==0)
+        {
+            Email.setError("Enter Email");
+        }
+
+        if(password.getText().toString().trim().length()==0)
+        {
+            password.setError("Enter Password");
+        }
+
+        if(ConfirmPassword.getText().toString().trim().length()==0)
+        {
+            ConfirmPassword.setError("Enter Confirmed Password");
+        }
+
+
+
+        if(FirstName.getText().toString().trim().length()!=0 && LastName.getText().toString().trim().length()!=0 && Email.getText().toString().trim().length()!=0 && password.getText().toString().trim().length()!=0 && ConfirmPassword.getText().toString().trim().length()!=0) {
+                if (password.equals(ConfirmPassword)) {
+                    User user = new User(FirstName.getText().toString(), LastName.getText().toString(), Email.getText().toString(), password.getText().toString(), ConfirmPassword.getText().toString(), 1);
+                    // Toast.makeText(UserRegistration.this, user.toString(), Toast.LENGTH_SHORT).show();
+                    DbHelper dbHelper = new DbHelper(UserRegistration.this);
+                    boolean b = dbHelper.addUser(user);
+                    if (b == true)
+                        Toast.makeText(UserRegistration.this, "uSER aDDED", Toast.LENGTH_SHORT).show();
+                    else
+                        Toast.makeText(UserRegistration.this, "User Not ADDED", Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(UserRegistration.this, "Password Not Matched", Toast.LENGTH_SHORT).show();
+            }
     }
 }
