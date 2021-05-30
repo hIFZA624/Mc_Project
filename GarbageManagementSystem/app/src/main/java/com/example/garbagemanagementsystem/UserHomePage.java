a5ccab9b4f0e093961b255325f3e0cdbb3db5aa2
hIFZA624 <mcsf19m020@pucit.edu.pk>
2021-05-30 09:58:53 +0500

    Detaisl"

---
+++ GarbageManagementSystem/app/src/main/java/com/example/garbagemanagementsystem/UserHomePage.java
@@ -0,0 +1,34 @@
+package com.example.garbagemanagementsystem;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class UserHomePage extends AppCompatActivity {
+    NavigationView nav;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_home_page);
+        toolbar = findViewById(R.id.toolbar2);
+        nav = findViewById(R.id.navmenu2);
+        drawerLayout = findViewById(R.id.fwareer);
+        toggle=new  ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+    public void OpenComplaint(View view) {
+        Intent intent=new Intent(UserHomePage.this,AddComplaint.class);
+        startActivity(intent);
+    }
+}
